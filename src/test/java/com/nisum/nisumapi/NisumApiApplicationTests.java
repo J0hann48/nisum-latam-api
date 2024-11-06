3b2291dfc25ab2fe14b17297661dc4e21dbca958
Johann Torres <joahnn.1996@gmail.com>
2024-11-05 22:38:06 -0500

    first commit

---
+++ src/test/java/com/nisum/nisumapi/NisumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisum.nisumapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NisumApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
